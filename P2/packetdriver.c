f09ffb721a9f5c7bacc72efd7584d6c08e713f5a
CIS Student <me@debian-BULLSEYE-live-builder-AMD64>
2022-05-08 23:25:44 -0700

    Code works, design diagram needed

--- P2/packetdriver.c
+++ P2/packetdriver.c
@@ -1,3 +1,10 @@
+/*
+ * AUTHOR : AHMET TURAN BULUT
+ * LOGIN : aturanb
+ * TITLE: CIS 415 Project 2
+ * This is my own work except for help hours from Timmy & Nick, and Pseudocode that was given during lab hours.
+*/
+
 #include <pthread.h>
 #include "BoundedBuffer.h"
 #include "destination.h"
@@ -16,6 +23,7 @@
 
 #define TRY 15
 
+//Global Variables
 BoundedBuffer *buf_pool;
 BoundedBuffer *buf_receive [MAX_PID + 1];
 BoundedBuffer *buf_send;
@@ -23,34 +31,40 @@ BoundedBuffer *buf_send;
 FreePacketDescriptorStore *fpds;
 NetworkDevice *networkDevice;
 
+//Helper Functions
 static void *send();
 static void *receive();
 
 void init_packet_driver(NetworkDevice *nd, void *mem_start, unsigned long mem_length, FreePacketDescriptorStore **fpds_ptr){
-	/* create Free Packet Descriptor Store using mem_start and mem_length */
+	//Create Free Packet Descriptor Store using mem_start and mem_length
 	fpds = FreePacketDescriptorStore_create(mem_start, mem_length);
 	
-	/* create any buffers required by your thread[s] */
+	//Create any buffers required by send and receive threads 
 	buf_pool = BoundedBuffer_create(MAX_PID);
+	if(buf_pool == NULL) { DIAGNOSTICS("[Driver> ERROR: Failed to create Pool Buffer.\n"); }
+	
 	buf_send = BoundedBuffer_create(MAX_PID);
+	if(buf_send == NULL) { DIAGNOSTICS("[Driver> ERROR: Failed to create Send Buffer.\n"); }
+	
 	for(int i = 0; i < MAX_PID + 1; i++){
 		buf_receive[i] = BoundedBuffer_create(MAX_PID);
+		if(buf_receive[i] == NULL) { 
+			DIAGNOSTICS("[Driver> ERROR: Failed to create Receive[%d] Buffer.\n", i); 
+		}
 	}
 
 	/* return the FPDS to the code that called you */
 	networkDevice = nd;
 	*fpds_ptr = (FreePacketDescriptorStore *) fpds;
-	DIAGNOSTICS("SIZE OF FPDS: %ld\n", fpds->size(fpds));
 	
 	/* create any threads you require for your implementation */
 	pthread_t senderT, receiverT;
-	pthread_create(&senderT, NULL, send, NULL);
-	pthread_create(&receiverT, NULL, receive, NULL);
-
-
-
-
-	
+	if(pthread_create(&senderT, NULL, send, NULL) != 0){
+		DIAGNOSTICS("[Driver> ERROR: Failed to create Send Thread.\n");
+	}
+	if(pthread_create(&receiverT, NULL, receive, NULL) != 0){
+		DIAGNOSTICS("[Driver> ERROR: Failed to create Receive Thread.\n");
+	}
 }
 
 void blocking_send_packet(PacketDescriptor *pd) {
@@ -81,72 +95,100 @@ int nonblocking_get_packet(PacketDescriptor **pd, PID pid) {
 
 //Sends a packet descriptor to network
 static void *send(){
+	
 	PacketDescriptor *pd = NULL;
 	int i;
+	
 	while(1){
 		
 		//Get Packet Descriptor from send buffer
 		buf_send->blockingRead(buf_send, (void **) &pd);
 		
-		//try sending the packet descriptor to Network Device
+		//Try sending the packet descriptor to Network Device
 		for(i = 0; i < TRY; i++){
 			if(networkDevice->sendPacket(networkDevice, pd)){
-				DIAGNOSTICS("PACKETDRIVER: Packet Sent to Network\n");
+				DIAGNOSTICS("[Driver> Packet sent after %d tries.\n", i + 1);
 				break;
 			}
 			else if(i == (TRY - 1)){
-				DIAGNOSTICS("PACKETDRIVER: Packet Send Failed\n");
+				DIAGNOSTICS("[Driver> Sending packet failed.\n");
 			}
-
-
-
 		}
 		
-		//Send it to pool 
+		//Send the packet descriptor to pool, if it fails send it to FPDS
 		if(!buf_pool->nonblockingWrite(buf_pool, pd)){
-			DIAGNOSTICS("PACKETDRIVER: Non Blocking Write to Buffer Pool failed.\n Sending Packet Descriptor to Free Packet Descriptor Store\n");
-			fpds->nonblockingPut(fpds, pd);
+			DIAGNOSTICS("[Driver> Non Blocking Write to Buffer Pool failed,\n");
+			DIAGNOSTICS("         Sending Packet Descriptor to Free Packet Descriptor Store.\n");
+			if(!fpds->nonblockingPut(fpds, pd)){
+				DIAGNOSTICS("[Driver> ERROR: Non Blocking Put to Free Packet Descriptor Store failed,\n");
+			}
 		}
 	}
 	return NULL;
-
 }
 
 //Receives a packet descriptor from the network
 static void *receive(){
-	PacketDescriptor *pd = NULL;
-	PID pid;
 
-	//Get the free packet descriptor from fpds
-	fpds->blockingGet(fpds, &pd);
+	PacketDescriptor *curr_pd = NULL;
+	PacketDescriptor *filled_pd = NULL;
+	PID pid;
 	
-	//Init the pd
-	initPD(pd);
+	//Get the free packet descriptor from fpds
+	fpds->blockingGet(fpds, &curr_pd);
+	
+	//Initialize the packet descriptor
+	initPD(curr_pd);
 	
 	//Tell the network device to use the indicated PacketDescriptor for next incoming data packet
-	networkDevice->registerPD(networkDevice, pd);
-
+	networkDevice->registerPD(networkDevice, curr_pd);
+	
 	while(1){
-		//Block the thread until pd has been filled with an incoming data packet
+		
+
+		//Block the thread until there is a packet
 		networkDevice->awaitIncomingPacket(networkDevice);
 
-		//Get the PID of the application
-		pid = getPID(pd);
+		//Make filled_pd point to curr_pd
+		filled_pd = curr_pd;
 
-		DIAGNOSTICS("PACKETDRIVER: Packet Received for PID: %d\n", pid);
+		//If we get a Packet Descriptor from pool
+		if(buf_pool->nonblockingRead(buf_pool, (void **)&curr_pd)){
+			DIAGNOSTICS("Received Packet Descriptor From Pool.\n");
+			
+			//Initialize curr_pd
+			initPD(curr_pd);
 
-		//Write to buf_receive[pid];
-		buf_receive[pid]->blockingWrite(buf_receive[pid], pd);
+			//Tell the network device to use the indicated PacketDescriptor for next incoming data packet
+			networkDevice->registerPD(networkDevice, curr_pd);
+			
+			//Get the PID of the application
+			pid = getPID(filled_pd);
+			DIAGNOSTICS("[Driver> Packet Received for PID: %d\n", pid);
+			
+			//Write to buf_receive of the application, if it fails send it to pool, if that fails send it to FPDS
+			if(!buf_receive[pid]->nonblockingWrite(buf_receive[pid], curr_pd)){
+				DIAGNOSTICS("[Driver> Non Blocking Write to Buffer Receive failed for PID: %d,\n", pid);
+				DIAGNOSTICS("         Sending Packet Descriptor to Buffer Pool.\n");
+				if(!buf_pool->nonblockingWrite(buf_pool, filled_pd)){
+					DIAGNOSTICS("[Driver> Non Blocking Write to Buffer Pool failed for PID: %d,\n", pid);
+					DIAGNOSTICS("         Sending Packet Descriptor to Free Packet Descriptor Store.\n");
+					if(!fpds->nonblockingPut(fpds, filled_pd)){
+						DIAGNOSTICS("[Driver> ERROR: Non Blocking Put to Free Packet Descriptor Store failed,\n");
+					}
+				}
+			}
+			
+		}
 
-		
-		//Reset the PD with initPD
-		initPD(pd);
-		
-		//Return back to the store or pool???
-		fpds->blockingPut(fpds, pd);
-
-
+		//If we don't get a Packet Descriptor from pool:
+		else {
+			curr_pd = filled_pd;
+			//Initialize and register curr_pd for next coming packet
+			initPD(curr_pd);
+			networkDevice->registerPD(networkDevice, curr_pd);
+		}
 	}
-	return NULL;
 	
+	return NULL;
 }
